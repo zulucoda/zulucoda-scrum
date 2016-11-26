a92770dba92d71bd4bbf66229cb6c2da65109d56
Muzikayise Flynn Buthelezi aka zuluCoda <muzi@mfbproject.co.za>
2016-11-26 23:01:47 +0200

    adding right template css, add styling the walls and stories pages

--- src/app/app.js
+++ src/app/app.js
@@ -4,7 +4,10 @@ import uiRouter from 'angular-ui-router';
 
 import walls from './../features/walls'
 
-import '../style/app.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/font-awesome/css/font-awesome.css';
+import '../style/lib/right.dark.css';
+import 'font-awesome-webpack';
 
 const MODULE_NAME = 'zulucoda.scrum';
 
